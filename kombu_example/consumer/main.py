b46c5af13e3a001d4ecc7d5210d668fbbac8f558
沈嘉诚 <976739120@qq.com>
2018-09-04 18:19:35 +0800

    提交

--- kombu_example/consumer/main.py
+++ kombu_example/consumer/main.py
@@ -2,21 +2,27 @@
 
 from kombu_example.consumer.config import config_use
 from kombu_example.consumer.consumer import ConsumerRoutes
-KR = ConsumerRoutes(config_use.get_connection())
-
+import multiprocessing
+import os
+import threading
 if __name__ == '__main__':
-    def do(body, message):
-        print(1111)
+    #
+    def a():
+        KR = ConsumerRoutes(config_use.get_connection(), 2)
 
+        @KR.route(exchange_name="BExchange", queue_name="BQueue", routing_key="b")
+        def print_info(body, message):
+            print("B线程", threading.currentThread().ident)
+            print('B进程', os.getpid(), os.getppid())
+            print(threading.active_count())
+            message.ack()
 
-    @KR.route(exchange_name="AExchange", queue_name="AQueue", routing_key="a")
-    def print_info(body, message):
-        print('body : "{0}"'.format(body))
-        message.ack()
+        @KR.route(exchange_name="AExchange", queue_name="AQueue", routing_key="a")
+        def print_info(body, message):
+            print("A线程", threading.currentThread().ident)
+            print('A进程', os.getpid(), os.getppid())
+            message.ack()
 
-
-    @KR.route(exchange_name="BExchange", queue_name="BQueue", routing_key="b")
-    def print_info(body, message):
-        print('body : "{0}"'.format(body))
-
-        message.ack()
+    for i in range(2):
+        p = multiprocessing.Process(target=a)
+        p.start()
